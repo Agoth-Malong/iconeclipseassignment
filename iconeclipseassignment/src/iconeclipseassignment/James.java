28aa27889b06960b162e8e4cac862773410eba95
Agoth-Malong <113917044+Agoth-Malong@users.noreply.github.com>
2022-10-01 15:12:30 +0300

    initial commit

---
+++ iconeclipseassignment/src/iconeclipseassignment/James.java
@@ -0,0 +1,17 @@
+package iconeclipseassignment;
+
+public class James
+{
+	public static void main(String[]args)
+	{
+		int a = 1000;
+		int b = 2000;
+		int c = 3000;
+		
+		int d = a + b; 
+		int e = d * c;
+		
+		System.out.println("|The Final Multiplication is:| " + e);
+	}
+
+}
---
+++ iconeclipseassignment/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author MASTER DUNIA
+ *
+ */
+module iconeclipseassignment {
+}
